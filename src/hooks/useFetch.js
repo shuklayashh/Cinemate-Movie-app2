535dfb53be5620ce8dd7be73ffa8328d77cb838d
Yash Shukla <shuklayash457@gmail.com>
2023-10-03 18:32:58 +0530

    Complete Code

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=81ad184ef3feb48d16ad28b8619b2093&query=${queryTerm}`
+console.log(data);
+    useEffect(() => {
+        async function fetchMovies(){
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+        }
+        fetchMovies();
+      }, [url])
+
+  return { data }
+}
