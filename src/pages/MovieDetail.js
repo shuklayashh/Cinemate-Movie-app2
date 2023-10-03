da4a9904982137f264da632e99c0f53a1410f373
Yash Shukla <shuklayash457@gmail.com>
2023-10-03 22:17:56 +0530

    Updated pageTitle

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -5,7 +5,7 @@ import {useTitle} from "../hooks/useTitle";
 export const MovieDetail = () => {
   const params = useParams();
   const [movie,movieData] = useState({});
-  //esLint-disable-next-Line
+    //eslint-disable-next-line
   const pageTitle = useTitle()
   const image = movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : Backup ;
 
